47bc8a7d62483b4cfd04f2b759d48c91f8372263
emirm35 <emirmrt35@gmail.com>
2022-06-28 21:36:37 +0300

    Vize sınavından 50 , Final sınavından 80 almığ öğrencinin not ortalamasını gösteren buton. (vize %20 - final %80)

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+function changeCss(){
+   
+    
+   var vize=50;
+   var final=80;
+   var ortalama=0;
+
+   
+   ortalama = ((vize * 20) / 100) + ((final * 80) / 100);
+
+   document.getElementById('sayi').innerHTML=ortalama;
+}
